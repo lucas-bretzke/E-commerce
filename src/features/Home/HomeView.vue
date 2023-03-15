fdd0ea7ea2f3c90bea554f9211fc3f48496aa72d
lucas bretzke <lucasbretzke@gmail.com>
2023-03-15 02:35:02 -0300

    added: navBar, Header

---
+++ src/features/Home/HomeView.vue
@@ -0,0 +1,101 @@
+<template>
+  <main class="home">
+    <div class="background">
+      <NavBar class="navBar">
+        <template #Left> <span class="logo">Bretzke Car</span> </template>
+        <template #Right>
+          <li style="">
+            <a href="" style="cursor: pointer">Ajuda</a>
+            <a href="">Contato</a>
+            <a href="">About</a>
+            <a href="">Carrinho</a>
+            <button>Contato</button>
+          </li>
+        </template>
+      </NavBar>
+      <Header class="center" />
+    </div>
+
+    <InputLarge />
+  </main>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import NavBar from "@/components/baseNavBar/BaseNavBar.vue";
+import Header from "./components/Header/Header.vue";
+import InputLarge from "./components/InputLarge.vue";
+
+@Options({
+  components: {
+    Header,
+    InputLarge,
+    NavBar,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
+
+<style scoped lang="less">
+.home {
+  display: flex;
+  flex-direction: column;
+}
+
+.center {
+  max-width: 1000px;
+  min-width: 300px;
+  margin: 0px auto;
+}
+
+.navBar {
+  list-style: none;
+  // position: fixed;
+  width: 1000px;
+  max-width: 1000px;
+  margin: 0px auto;
+  background-color: var(--orange);
+
+  .logo {
+    padding: 50px 0px;
+    font-size: 22px;
+  }
+
+  a {
+    padding: 10px 20px;
+    font-size: 16px;
+    color: var(--blue);
+    text-decoration: none;
+    cursor: pointer;
+    transition: 0.5s;
+  }
+
+  a:hover {
+    color: white;
+  }
+
+  button {
+    padding: 10px 35px;
+    font-size: 16px;
+    color: var(--blue);
+    cursor: pointer;
+    border-radius: 5px;
+    border: solid 1px black;
+    background-color: var(--orange);
+  }
+
+  button:hover {
+    transition: 0.5s;
+    color: white;
+    background-color: var(--blue);
+  }
+}
+
+.background {
+  width: 100%;
+  height: 500px;
+  margin: 0px 0px 200px 0px;
+  align-items: center;
+  background-color: #f78b00;
+}
+</style>
