92eba335bf82b3aeed51280aae86e79d19e6399f
lucas bretzke <lucasbretzke@gmail.com>
2023-03-15 02:37:13 -0300

    refactor: header file location changed

--- src/features/Home/HomeView.vue
+++ src/features/Home/HomeView.vue
@@ -23,7 +23,7 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import NavBar from "@/components/baseNavBar/BaseNavBar.vue";
-import Header from "./components/Header/Header.vue";
+import Header from "./components/Header.vue";
 import InputLarge from "./components/InputLarge.vue";
 
 @Options({
