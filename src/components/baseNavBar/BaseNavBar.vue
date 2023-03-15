a2756f69c4e44068ff3693d4c30ea8aff3a1144d
lucas bretzke <lucasbretzke@gmail.com>
2023-03-15 00:18:41 -0300

    feat: base navBar

---
+++ src/components/baseNavBar/BaseNavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <slot class="left" name="Left"></slot>
+    <slot class="center" name="Center"></slot>
+    <slot class="right" name="Right"></slot>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class NavBar extends Vue {}
+</script>
+
+<style scoped lang="less">
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .left,
+  .center,
+  .right {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
