5deb986d59bf9b628ccbe713a182fcf8a1ea585b
lucas bretzke <lucasbretzke@gmail.com>
2023-03-15 03:42:26 -0300

    refactor: unnecessary code deleted

--- src/features/Home/components/InputLarge.vue
+++ src/features/Home/components/InputLarge.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="center">
-    <div class="container">
+  <div class="background">
+    <section class="container">
       <input type="text" placeholder="Choose favorite model" />
       <button>Search</button>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -15,57 +15,47 @@ export default class InputLarge extends Vue {}
 </script>
 
 
-<style scoped>
-:root {
-  --orange: #f78b00;
-  --blue: #00285f;
-  --gray: #ededed;
-}
-
-.center {
+<style scoped lang="less">
+.background {
   max-width: 1000px;
-  min-width: 300px;
-
-  margin: 0px auto;
-  padding: 0px;
-
+  width: 1000px;
+  margin: 0 auto;
+  padding: 0;
   border-radius: 5px;
   background-color: var(--gray);
-}
 
-.container {
-  padding: 40px;
+  .container {
+    padding: 40px;
+    display: flex;
+    border-radius: 5px;
 
-  display: flex;
-  border-radius: 5px;
-}
+    input {
+      width: 100%;
+      padding: 10px;
+      border: none;
+      border-radius: 5px 0px 0px 5px;
+      outline: none;
+    }
 
-input {
-  width: 100%;
-  padding: 10px;
+    button {
+      width: 20%;
+      padding: 15px;
+      font-size: 16px;
+      border: none;
+      border-radius: 0px 5px 5px 0px;
+      color: white;
+      background-color: var(--blue);
+      transition: 0.5s;
+      border: 1px solid transparent;
+    }
 
-  border: none;
-  border-radius: 5px 0px 0px 5px;
-
-  outline: none;
-}
-
-button {
-  width: 20%;
-  padding: 15px;
-
-  font-size: 16px;
-
-  border: none;
-  border-radius: 0px 5px 5px 0px;
-
-  color: white;
-  background-color: var(--blue);
-  transition: 0.5s;
-}
-
-button:hover {
-  color: var(--blue);
-  background-color: var(--gray);
+    button:hover {
+      cursor: pointer;
+      font-weight: 600;
+      border: 1px solid var(--blue);
+      color: var(--blue);
+      background-color: var(--gray);
+    }
+  }
 }
 </style>
