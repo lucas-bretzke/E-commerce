50ba8cd0979d36ee38cb98e14c37baff9be69dbf
lucas bretzke <lucasbretzke@gmail.com>
2023-03-16 01:35:23 -0300

    feat: installed Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCartShopping)
+
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount('#app')
