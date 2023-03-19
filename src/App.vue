6a596ec8efe7cd085d326c1d31738cc2a0a176dc
lucas bretzke <lucasbretzke@gmail.com>
2023-03-19 04:52:26 -0300

    feat: added --color-terciary

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,14 @@
 
 <style lang="less">
 :root {
-  --color-primary: #d2b48c;
+  --color-primary: #d2b48c; //Marrom Rosado
   --color-secundary: black;
+  --color-terciary: #bc8f8f; //Marrom Coral
   --gray: #ededed;
-//   #F0E68C - Amarelo Areia
-// #D2B48C - Marrom Rosado
-// #BC8F8F - Marrom Coral
-// #8B4513 - Marrom Tabaco
-// #6B8E23 - Verde Oliva
-// #228B22 - Verde Floresta
+  //   #F0E68C - Amarelo Areia
+  // #8B4513 - Marrom Tabaco
+  // #6B8E23 - Verde Oliva
+  // #228B22 - Verde Floresta
 }
 
 *,
