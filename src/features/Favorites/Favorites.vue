62bf05c6b95b4513e3bb19ae5c38938e6e6dc45e
Lucas Bretzke <lucasbretzke@gmail.com>
2023-03-28 21:06:55 -0300

    feat: Basic bookmark page done

---
+++ src/features/Favorites/Favorites.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1>Favorites</h1>
+  <main class="center">
+    <section class="catalog-filters"></section>
+    <section class="container-cards">
+      <div v-for="item in dataItems" :key="item">
+        <BaseCardItems v-if="item.favorite" :item="item" />
+      </div>
+    </section>
+  </main>
+</template>
+
+
+<script lang="ts">
+import { IBlusa } from "@/types";
+import productService from "@/services/productService";
+import { Options, Vue } from "vue-class-component";
+import BaseCardItems from "@/components/BaseCardItem.vue";
+
+@Options({ components: { BaseCardItems } })
+export default class Favorites extends Vue {
+  public dataItems: IBlusa[] = [];
+
+  private async getItems() {
+    try {
+      const response = await productService.get();
+      this.dataItems = response;
+      console.log("get favorites");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  mounted() {
+    this.getItems();
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+h1 {
+  width: 1200px;
+  margin: 40px auto;
+  display: flex;
+  font-size: 35px;
+}
+.center {
+  max-width: 1200px;
+  display: flex;
+  margin: 0 auto;
+  justify-content: space-between;
+  border: 1px solid red;
+
+  .catalog-filters {
+    width: 228px;
+    height: auto;
+    max-height: 1000px;
+    background-color: #4444;
+  }
+  .container-cards {
+    max-width: 970px;
+    height: auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    border: 1px solid blue;
+  }
+}
+</style>
