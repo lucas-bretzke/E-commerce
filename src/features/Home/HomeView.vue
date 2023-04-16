78d42863ec58263e31cd7019216ebb020dc68ce4
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-15 23:01:32 -0300

    refactor: deleted unnecessary code

--- src/features/Home/HomeView.vue
+++ src/features/Home/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <main class="home">
     <div class="background-top">
-    
       <header class="center header-block">
         <ImageSlider
           :images="images"
@@ -11,14 +10,21 @@
         />
       </header>
     </div>
-    <ProductCarousel title="NOVIDADES" />
+    <ProductCarousel
+      title="NOVIDADES"
+      :appearPromotion="false"
+      :dataItems="dataBlouses"
+      @getItems="getBlouses"
+    />
   </main>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
 import ImageSlider from "../../components/ImageSlider.vue";
 import ProductCarousel from "@/components/ProductCarousel.vue";
+import productService from "@/services/productService";
+import { IBlusa, IShorts } from "@/types";
+import { Options, Vue } from "vue-class-component";
 
 @Options({
   components: {
@@ -27,7 +33,7 @@ import ProductCarousel from "@/components/ProductCarousel.vue";
   },
 })
 export default class HomeView extends Vue {
-  images = [
+  public images = [
     {
       url: "/img/roupas03.jpg",
       alt: "Imagem 1",
@@ -41,6 +47,25 @@ export default class HomeView extends Vue {
       alt: "Imagem 3",
     },
   ];
+  public dataBlouses: IBlusa[] = [];
+  public dataShorts: any[] = [];
+
+  public async getBlouses() {
+    try {
+      const response = await productService.getBlouses();
+      this.dataBlouses = response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  public async getShorts() {
+    try {
+      const response = await productService.getShorts();
+      this.dataShorts = response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 </script>
 
@@ -62,8 +87,6 @@ export default class HomeView extends Vue {
 .home {
   display: flex;
   flex-direction: column;
-
-  
 
   .header-block {
     width: 100%;
