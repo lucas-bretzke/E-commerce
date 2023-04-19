8b70415c917950334e32020c1be381f1100bfd8c
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-19 16:01:33 -0300

    refactored: class names, and css

--- src/components/Cookies.vue
+++ src/components/Cookies.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="cookie-modal">
     <h1 class="title">Este site usa Cookies</h1>
-    <button class="close" @click="closeModal">X</button>
+    <button class="btn-close" @click="closeModal">X</button>
     <p>
       Utilizamos cookies para melhorar sua experiência em nosso site e podermos
       sugerir conteúdo relevante para você. Ao continuar, você concorda e aceita
       nossa
     </p>
     <a @click="pageFake">Política de Privacidade</a>
-    <button class="btn-aceitar" @click="toCcept">Aceitar</button>
-    <button class="btn-recusar">Opcões</button>
+    <button class="btn-accept" @click="toCcept">Aceitar</button>
+    <button class="btn-refuse">Recusar</button>
   </section>
 </template>
 
@@ -20,13 +20,14 @@ import { Options, Vue } from "vue-class-component";
 @Options({})
 export default class Cookies extends Vue {
   $store: any;
+
   public closeModal() {
-    this.$store.state.myMod.isCookie = false;
+    this.$store.state.cookieAcceptanceModal = false;
   }
 
   public toCcept() {
-    this.closeModal();
     alert("Cookies aceitos!");
+    this.closeModal();
   }
 
   public pageFake() {
@@ -36,74 +37,73 @@ export default class Cookies extends Vue {
 </script>
 
 
-
 <style lang="less" scoped>
+@base-btn: {
+  padding: 7px;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+    transform: translateZ(0px) scale(101%);
+    transition: 0.3s;
+  }
+};
 .cookie-modal {
   width: 400px;
   height: auto;
-
   position: fixed;
-  top: 50%;
+  bottom: 1%;
   left: 1%;
-
   display: flex;
   flex-direction: column;
   padding: 30px;
-
   border-radius: 1px;
   background-color: white;
-
   box-shadow: 5px 10px 20px;
+  z-index: 10;
 
   .title {
     font-weight: bold;
     margin-bottom: 15px;
   }
-  .close {
+  .btn-close {
     font-size: 20px;
     border: none;
     background-color: transparent;
-
     position: absolute;
     left: 90%;
     top: 3%;
-  }
-  .close:hover {
-    transform: translateZ(0px) scale(1.3);
-    transition: 0.3s;
+
+    &:hover {
+      cursor: pointer;
+      transform: translateZ(0) scale(120%);
+    }
   }
   p {
     font-size: 14px;
   }
   a {
+    width: 52%;
     margin: 10px 0px;
     font-weight: bold;
+    border-bottom: 1px solid transparent;
     color: rgb(1, 115, 201);
-  }
-  a:hover {
-    cursor: pointer;
-    border-bottom: 1px solid;
-    width: 52%;
+
+    &:hover {
+      cursor: pointer;
+      border-bottom: 1px solid rgb(1, 115, 201);
+    }
   }
 
-  button {
-    padding: 7px;
-    font-weight: bold;
-  }
-
-  button:hover {
-    cursor: pointer;
-    transform: translateZ(0px) scale3d(1, 1.1, 1.1);
-  }
-  .btn-aceitar {
+  .btn-accept {
+    @base-btn();
     background-color: white;
     border: 2px solid black;
     margin-bottom: 10px;
   }
-  .btn-aceitar:hover {
-    text-shadow: 0px 0px 23px 48px rgba(0, 0, 0, 0.22);
-  }
-  .btn-recusar {
+
+  .btn-refuse {
+    @base-btn();
     color: white;
     background-color: black;
     border: 2px solid black;
