e2adcd92a56ccea3e20fc9ddec2d6f740a92407c
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-20 04:12:03 -0300

    feat: added rout details

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../features/Home/HomeView.vue'
 import Favorites from '../features/Favorites/Favorites.vue'
+import ItemDetail from '../features/ItemDetail/ItemDetails.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Favorites',
     component: Favorites
   },
+  {
+    path: '/Details',
+    name: 'Details',
+    component: ItemDetail
+  }
 ]
 
 const router = createRouter({
