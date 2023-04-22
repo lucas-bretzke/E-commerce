2ccb55f2cf4e27911c322f1e67800c4174589a66
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-22 02:56:11 -0300

    added FontAwesomeIcon brands

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import router from './router'
 import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp, } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 
 library.add(faCartShopping, faArrowRight, solidHeart, regularHeart, faChevronRight, faChevronLeft, faCaretUp)
 
