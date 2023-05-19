3392c0d64693d58f38b6fc3555b6c955b3bf8f8e
Lucas Bretzke <lucasbretzke@gmail.com>
2023-05-19 06:36:07 -0300

    feat: added user data fields to store

--- src/stores/baseStore.ts
+++ src/stores/baseStore.ts
@@ -3,6 +3,15 @@ import { defineStore } from 'pinia'
 export const baseStore = defineStore('my-baseStore', {
     state: () => {
         return {
+            user: {
+                name: '',
+                email: '',
+                password: '',
+                phone: '',
+                contactPreference: '',
+                address: '',
+            },
+            isLogin: false,
             itemIdInDetail: 0,
             favoritesCounter: 0,
             cartCounter: 0,
