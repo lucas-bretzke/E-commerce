7ae868b317ff431707d72afdb75efb8a1bd0423d
Lucas Bretzke <lucasbretzke@gmail.com>
2023-05-21 23:54:51 -0300

    feat: added icons: faEyeSlash, faEye

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,14 @@ import firebase from 'firebase/app'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHeart as regularHeart, } from '@fortawesome/free-regular-svg-icons'
-import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as regularHeart, faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp, faSpinner, faG } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-
-library.add(faCartShopping, faArrowRight, solidHeart, regularHeart, faChevronRight, faChevronLeft, faCaretUp, faSpinner)
+library.add(
+    faCartShopping, faArrowRight, solidHeart, regularHeart, faChevronRight, faChevronLeft,
+    faCaretUp, faSpinner, faEye, faEyeSlash, faG
+)
 
 
 createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).use(createPinia()).use(router).mount('#app')
