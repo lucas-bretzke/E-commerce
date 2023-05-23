9f7291d02c57209f682c5ae39d98ea92793d3031
Lucas Bretzke <lucasbretzke@gmail.com>
2023-05-22 23:48:20 -0300

    feat: added icons

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ import firebase from 'firebase/app'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHeart as regularHeart, faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons'
-import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp, faSpinner, faG } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faHeart as regularHeart, faEye, faEyeSlash, } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping, faArrowRight, faHeart as solidHeart, faChevronRight, faChevronLeft, faCaretUp, faSpinner, } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 library.add(
     faCartShopping, faArrowRight, solidHeart, regularHeart, faChevronRight, faChevronLeft,
-    faCaretUp, faSpinner, faEye, faEyeSlash, faG
+    faCaretUp, faSpinner, faEye, faEyeSlash, faGoogle
 )
 
 
