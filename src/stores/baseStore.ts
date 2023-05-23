18e0ce35c5b276c57c414cbcedb0c48e636d82b4
Lucas Bretzke <lucasbretzke@gmail.com>
2023-05-23 18:02:14 -0300

    feat: added checkUser to store

--- src/stores/baseStore.ts
+++ src/stores/baseStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { Auth } from "@/firebase";
 
 export const baseStore = defineStore('my-baseStore', {
     state: () => {
@@ -11,7 +12,7 @@ export const baseStore = defineStore('my-baseStore', {
                 contactPreference: '',
                 address: '',
             },
-            isLogin: false,
+            isLogin: true,
             itemIdInDetail: 0,
             favoritesCounter: 0,
             cartCounter: 0,
@@ -19,4 +20,19 @@ export const baseStore = defineStore('my-baseStore', {
             getDone: false
         }
     },
+    actions: {
+        checkUser() {
+            Auth.onAuthStateChanged((user) => {
+                if (user) {
+                    console.log("Usuário logado.");
+                    this.user.name === user.displayName
+                    this.user.email === user.email
+                    this.isLogin = false;
+                } else {
+                    console.log("Nenhum usuário logado.");
+                }
+            });
+        }
+    },
+    getters: {}
 })
