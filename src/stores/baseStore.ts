3bca403f5edf674cc14ea67b8a90411470b12135
Lucas Bretzke <lucasbretzke@gmail.com>
2024-11-02 20:28:00 -0300

    feat: added new states in the store, functions to calculate values ​​and save information in localStorage, product update function

--- src/stores/baseStore.ts
+++ src/stores/baseStore.ts
@@ -1,126 +1,177 @@
 import productApi from '@/services/productApi'
 import { defineStore } from 'pinia'
-import { Product, Order } from '@/types'
-
-// Definindo a interface para os métodos de pagamento
-interface PaymentMethod {
-  id: number
-  name: string
-}
+import { Product, PaymentMethod, Order } from '@/types'
 
 export const baseStore = defineStore('my-baseStore', {
   state: () => ({
     user: {
       name: '',
       email: '',
-      password: '',
       phone: '',
-      contactPreference: '',
-      address: '',
-      uid: ''
+      address: {
+        city: '',
+        state: '',
+        street: '',
+        country: '',
+        postalCode: '323444'
+      },
+      uid: '',
+      contactPreference: ''
     },
-    baseProducts: [] as Product[],
-    productsInCart: [] as Product[],
-    showLoginScreen: false,
-    favoritesCounter: 0,
+    order: JSON.parse(localStorage.getItem('orderData') || '{}') as Order,
+    password: '',
+    couponCode: '',
     cartCounter: 0,
     cookieAcceptanceModal: false,
-    paymentMethods: [] as PaymentMethod[], // Métodos de pagamento com o tipo definido
-    orderHistory: [] as Order[], // Histórico de pedidos
-    couponCode: '', // Código do cupom
-    appliedCoupon: null // Detalhes do cupom aplicado
+    orderHistory: [] as Order[],
+    baseProducts: [] as Product[],
+    appliedCoupon: null as { code: string; discount: string } | null,
+    paymentMethods: [] as PaymentMethod[],
+    productsInCart: JSON.parse(
+      localStorage.getItem('cartItems') || '[]'
+    ) as Product[],
+    showLoginScreen: false,
+    favoritesCounter: 0
   }),
-  getters: {},
+
   actions: {
-    checkUser() {
-      this.user?.uid
-        ? console.log('User logged in.')
-        : console.log('No user logged in.')
+    setUserData(user: Partial<typeof this.user>) {
+      this.user = { ...this.user, ...user }
     },
 
-    setUserData(user: any) {
-      this.user = user
+    updateLocalStorage() {
+      localStorage.setItem('cartItems', JSON.stringify(this.productsInCart))
+      localStorage.setItem('orderData', JSON.stringify(this.order))
     },
 
-    async getAllProducts() {
-      try {
-        const data = await productApi.getBaseProducts()
-        this.baseProducts = data
-
-        this.updateCounters()
+    async getAllProducts(forceUpdate = false) {
+      if (!forceUpdate && this.baseProducts.length) {
         this.dataCartItems()
+        this.updateCounters()
+        this.calculateTotalOrder()
+        return
+      }
+
+      try {
+        const cachedProducts = JSON.parse(
+          localStorage.getItem('baseProducts') || '[]'
+        )
+        if (cachedProducts.length && !forceUpdate) {
+          this.baseProducts = cachedProducts
+        } else {
+          const data = await productApi.getBaseProducts()
+          this.baseProducts = data
+          localStorage.setItem('baseProducts', JSON.stringify(data))
+        }
+
+        this.dataCartItems()
+        this.updateCounters()
+        this.calculateTotalOrder()
       } catch (error) {
-        console.error('Erro ao obter produtos:', error)
+        console.error('Error fetching products:', error)
       }
     },
 
     dataCartItems() {
       this.productsInCart = this.baseProducts.filter(
-        (product: Product) => product.cart === true
+        (product: Product) => product.cart
       )
+      this.calculateTotalCartValue()
     },
 
-    totalCartValue() {
-      return this.productsInCart
-        .reduce((total, product) => total + product.price, 0)
-        .toFixed(2)
+    async toggleProduct(item: Product, type: 'favorites' | 'cart') {
+      try {
+        const updatedProduct = { ...item, [type]: !item[type] }
+        await productApi.setProduct(item.id, updatedProduct)
+        this.updateProductLocally(item.id, updatedProduct)
+        this.updateCounters()
+      } catch (error) {
+        console.error(`Error updating product ${item.id}:`, error)
+      }
+    },
+
+    updateProductLocally(productId: number, updatedData: Product) {
+      const index = this.baseProducts.findIndex(
+        product => product.id === productId
+      )
+      if (index !== -1) {
+        this.baseProducts[index] = updatedData
+        this.dataCartItems()
+        this.calculateTotalOrder()
+      }
+    },
+
+    calculateTotalCartValue() {
+      const total = this.productsInCart.reduce(
+        (sum, product) => sum + product.price,
+        0
+      )
+      this.order.totalCartValue = parseFloat(total.toFixed(2))
+      this.updateLocalStorage()
+    },
+
+    calculateTotalOrder() {
+      this.calculateTotalCartValue()
+      const shippingCost = parseFloat(this.calculateShipping())
+      this.order.deliveryValue = shippingCost
+      this.order.total = parseFloat(
+        (this.order.totalCartValue + shippingCost).toFixed(2)
+      )
+      this.updateLocalStorage()
     },
 
     updateCounters() {
-      const favorites = this.baseProducts.filter(
+      this.favoritesCounter = this.baseProducts.filter(
         product => product.favorites
       ).length
-      const cart = this.baseProducts.filter(product => product.cart).length
-
-      this.favoritesCounter = favorites
-      this.cartCounter = cart
+      this.cartCounter = this.baseProducts.filter(
+        product => product.cart
+      ).length
     },
 
-    calculateShipping(cep: any) {
-      const faixasDeFrete = [
-        { minCep: 1000000, maxCep: 1999999, valorFrete: 10.0 },
-        { minCep: 2000000, maxCep: 2999999, valorFrete: 15.0 },
-        { minCep: 3000000, maxCep: 3999999, valorFrete: 20.0 },
-        { minCep: 4000000, maxCep: 4999999, valorFrete: 25.0 },
-        { minCep: 5000000, maxCep: 5999999, valorFrete: 30.0 },
-        { minCep: 6000000, maxCep: 6999999, valorFrete: 35.0 },
-        { minCep: 7000000, maxCep: 7999999, valorFrete: 40.0 },
-        { minCep: 8000000, maxCep: 8999999, valorFrete: 45.0 },
-        { minCep: 9000000, maxCep: 9999999, valorFrete: 50.0 },
-        { minCep: 10000000, maxCep: 19999999, valorFrete: 55.0 }
+    loadCartData() {
+      const savedProducts = JSON.parse(
+        localStorage.getItem('cartItems') || '[]'
+      )
+      const savedOrder = JSON.parse(localStorage.getItem('orderData') || '{}')
+
+      if (savedProducts.length) this.productsInCart = savedProducts
+      if (savedOrder && Object.keys(savedOrder).length) this.order = savedOrder
+
+      this.calculateTotalCartValue()
+    },
+
+    calculateShipping() {
+      const cep = this.user.address.postalCode
+      const shippingRates = [
+        { minCep: 1000000, maxCep: 1999999, value: 10.0 },
+        { minCep: 2000000, maxCep: 2999999, value: 15.0 },
+        { minCep: 3000000, maxCep: 3999999, value: 20.0 },
+        { minCep: 4000000, maxCep: 4999999, value: 25.0 },
+        { minCep: 5000000, maxCep: 5999999, value: 30.0 },
+        { minCep: 6000000, maxCep: 6999999, value: 35.0 },
+        { minCep: 7000000, maxCep: 7999999, value: 40.0 },
+        { minCep: 8000000, maxCep: 8999999, value: 45.0 },
+        { minCep: 9000000, maxCep: 9999999, value: 50.0 },
+        { minCep: 10000000, maxCep: 19999999, value: 55.0 }
       ]
 
-      const cepNumerico = parseInt(cep.replace(/\D/g, ''), 10)
+      const numericCep = parseInt(cep.replace(/\D/g, ''), 10)
 
-      for (let i = 0; i < faixasDeFrete.length; i++) {
-        const faixa = faixasDeFrete[i]
-        if (cepNumerico >= faixa.minCep && cepNumerico <= faixa.maxCep) {
-          return faixa.valorFrete.toFixed(2)
-        }
-      }
-
-      const defaul = 55
-      return defaul.toFixed(2)
-    },
-
-    applyCoupon(code: string) {
-      this.couponCode = code
-      if (code === 'PROMO10') {
-        const discount = (this.totalCartValue() * 0.1).toFixed(2)
-        this.appliedCoupon = { code, discount }
-      } else {
-        this.appliedCoupon = null
-        console.error('Cupom inválido!')
-      }
+      const shipping = shippingRates.find(
+        range => numericCep >= range.minCep && numericCep <= range.maxCep
+      )
+      return (shipping ? shipping.value : 55.0).toFixed(2)
     },
 
     addOrder(order: Order) {
       this.orderHistory.push(order)
+      this.updateLocalStorage()
     },
 
     listPaymentMethods() {
       this.paymentMethods = [
-        { id: 1, name: 'Cartão de Crédito' },
+        { id: 1, name: 'Credit Card' },
         { id: 2, name: 'Boleto' },
         { id: 3, name: 'PayPal' }
       ]
